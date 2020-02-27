c1476db0e2552cbb23921aad0dd18f45ca739b64
gusailov <gusailov@gmail.com>
2020-02-27 14:00:29 +0200

    make footer and header

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+			<footer id="site-footer"  >
+<p>CopyRight 2020</p>
+				
+
+			</footer><!-- #site-footer -->
+
+		
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<?php
+
+
+?><!DOCTYPE html>
+
+<html >
+
+	<head>
+
+		
+
+	</head>
+
+	<body>
+
+
+	<img src="<?php echo get_field('logo', 'option')['sizes']['thumbnail']; ?>" />
+
+<?php
+
+?>
+
+
+	<h1>Hello</h1>
+	<h2>
+	<?php
+function add($first_number, $second_number) {
+    return $first_number + $second_number;
+}
+$value = add(1, 3); // $value is set to 4
+echo $value; // show $value in the HTML/browser
+
+?>
+	</h2>
+	
+	
